f3df9a2bd71e20d093f9d209085c3cf0bdf53720
Daniel Stewart <ds191401@gmail.com>
2023-12-03 14:47:03 -0500

    separate data

--- learn/training_set.py
+++ learn/training_set.py
@@ -7,24 +7,27 @@ class TrainingSet:
         if type(data) != list:
             raise ValueError('data must be a list, found {}'.format(type(data).__name__))
 
-        self._data = np.array(data)
+        data = np.array(data)
 
-        dimensions = len(self._data.shape)
+        dimensions = len(data.shape)
         if  dimensions != 2:
             raise ValueError('data must be 2-dimensional, found {} dimensions'.format(dimensions))
         
-        columns = self._data.shape[1]
+        columns = data.shape[1]
         if columns < 2:
             raise ValueError('data must have at least 2 colums, found {}'.format(columns))
 
+        self._features = data[:, 0:-1]
+        self._targets = data[:, -1]
+
     @property
     def examples(self) -> int:
-        return self._data.shape[0]
+        return self._features.shape[0]
     
     @property
     def features(self) -> int:
-        return self._data.shape[1] - 1
+        return self._features.shape[1]
     
     @property
     def targets(self) -> np.ndarray:
-        return self._data[:, -1]
+        return self._targets
